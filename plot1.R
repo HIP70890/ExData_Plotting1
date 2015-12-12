13112c9e2227548141557275049794fd6b38594e
HIP70890 <christian.h.schroeder@googlemail.com>
2015-12-12 19:18:43 +0100

    version storing directly into png

--- plot1.R
+++ plot1.R
@@ -1,4 +1,9 @@
 library(data.table)
 source("uci.datasource.R")
 
-
+png("plot1.png")
+hist(UCI.data$Global_active_power,
+     main = "Global Active Power",
+     xlab = "Global Active Power (kilowatts)",
+     col = "red")
+dev.off()
